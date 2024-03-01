f5f5662f58a8fee8dfe75ea3d6529421cb9a159c
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-03-01 15:04:56 -0800

    Adding extension to wrap up OpenTelemetry configuration

---
+++ src/DotNetWorker.OpenTelemetry/FunctionsOpenTelemetryExtensions.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace Microsoft.Azure.Functions.Worker
+{
+    using System;
+
+    using global::Azure.Monitor.OpenTelemetry.AspNetCore;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    using OpenTelemetry.Logs;
+    using OpenTelemetry.Metrics;
+    using OpenTelemetry.Trace;
+
+    public static class FunctionsOpenTelemetryExtensions
+    {
+        /// <summary>
+        /// Configures OpenTelemetry for Functions.
+        /// </summary>
+        /// <param name="services">The collection of services.</param>
+        /// <param name="context">The host builder context.</param>
+        /// <returns>The collection of services.</returns>
+        public static IServiceCollection ConfigureFunctionsOpenTelemetry(this IServiceCollection services, HostBuilderContext context)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (bool.TryParse(Environment.GetEnvironmentVariable("OTEL_SDK_DISABLED") ?? bool.TrueString, out var b) && !b)
+            {
+                // This block should all go in an extension method w/in Functions worker lib
+                var otBuilder = services.AddOpenTelemetry().UseAzureMonitor();
+
+                // This line of code should be put
+                IConfigurationSection newRelicExporterConfigSection = context.Configuration.GetSection(ConfigurationPath.Combine("openTelemetry", "exporters", "newrelic"));
+                if (newRelicExporterConfigSection.Exists())
+                {
+                    otBuilder
+                        .WithTracing(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind))
+                        .WithMetrics(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind));
+
+                    services.AddLogging(b => b.AddOpenTelemetry(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind)));
+                }
+
+                // Lets the host know that the worker is sending logs to App Insights. The host will now ignore these.
+                services.Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerApplicationInsightsLoggingEnabled"] = bool.TrueString);
+            }
+
+            return services;
+        }
+    }
+}
--- src/DotNetWorker.OpenTelemetry/README.md
+++ src/DotNetWorker.OpenTelemetry/README.md
@@ -1,5 +1,29 @@
-# Microsoft.Azure.Functions.Worker.ApplicationInsights
+# Microsoft.Azure.Functions.Worker.OpenTelemetry
 
-This package adds extension methods and services to configure OpenTelemetry for use in Azure Functions .NET isolated applications.
+This package adds extension methods and services to configure Open Telemetry for use in Azure Functions .NET isolated applications.
+
+## Getting Started
+
+1. Add packages
+
+``` CSharp
+dotnet add package Microsoft.Azure.Functions.Worker.OpenTelemetry
+```
+
+2. Configure Open Telemetry
+
+``` CSharp
+services.ConfigureFunctionsOpenTelemetry();
+```
+
+## Logging
 
 This package does **not** add OpenTelemetry services directly. This must be done directly. Instead, this package only configures isolated application and resource detector.
+
+## In-Proc Comparison / Changes
+
+With this package changing the worker to send telemetry directly to application insights, custom `ITelemetryInitializer` or `ITelemetryProcessor` will only apply to worker-originating telemetry. Telemetry which originates from the host process will **not** be ran through the same telemetry pipeline. This means when compared to an in-proc functions app, you may see some telemetry items missing customizations performed in initializers or processors. These telemetry items have originated from the host.
+
+## Configuration
+
+See [this document](https://learn.microsoft.com/en-us/dotnet/core/diagnostics/observability-with-otel) on configuring Open Telemetry for dotnet applications.
