440850e1f398738963c020d10d51c01ec06f1fbb
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2024-03-04 17:06:46 -0800

    make method give back the ot builder

--- src/DotNetWorker.OpenTelemetry/FunctionsOpenTelemetryExtensions.cs
+++ src/DotNetWorker.OpenTelemetry/FunctionsOpenTelemetryExtensions.cs
@@ -7,24 +7,21 @@ namespace Microsoft.Azure.Functions.Worker
 
     using global::Azure.Monitor.OpenTelemetry.AspNetCore;
 
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
 
-    using OpenTelemetry.Logs;
-    using OpenTelemetry.Metrics;
-    using OpenTelemetry.Trace;
+    using OpenTelemetry;
 
     public static class FunctionsOpenTelemetryExtensions
     {
         /// <summary>
         /// Configures OpenTelemetry for Functions.
         /// </summary>
-        /// <param name="services">The collection of services.</param>
-        /// <param name="context">The host builder context.</param>
-        /// <returns>The collection of services.</returns>
-        public static IServiceCollection ConfigureFunctionsOpenTelemetry(this IServiceCollection services, HostBuilderContext context)
+        /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
+        /// <param name="context">The <see cref="HostBuilderContext"/>.</param>
+        /// <returns>An instance of <see cref="OpenTelemetryBuilder"/> if OpenTelemetry is enabled, otherwise null.</returns>
+        public static OpenTelemetryBuilder? ConfigureFunctionsOpenTelemetry(this IServiceCollection services, HostBuilderContext context)
         {
             if (services is null)
             {
@@ -33,25 +30,15 @@ namespace Microsoft.Azure.Functions.Worker
 
             if (bool.TryParse(Environment.GetEnvironmentVariable("OTEL_SDK_DISABLED") ?? bool.TrueString, out var b) && !b)
             {
-                // This block should all go in an extension method w/in Functions worker lib
                 var otBuilder = services.AddOpenTelemetry().UseAzureMonitor();
-
-                // This line of code should be put
-                IConfigurationSection newRelicExporterConfigSection = context.Configuration.GetSection(ConfigurationPath.Combine("openTelemetry", "exporters", "newrelic"));
-                if (newRelicExporterConfigSection.Exists())
-                {
-                    otBuilder
-                        .WithTracing(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind))
-                        .WithMetrics(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind));
-
-                    services.AddLogging(b => b.AddOpenTelemetry(o => o.AddOtlpExporter(newRelicExporterConfigSection.Bind)));
-                }
 
                 // Lets the host know that the worker is sending logs to App Insights. The host will now ignore these.
                 services.Configure<WorkerOptions>(workerOptions => workerOptions.Capabilities["WorkerApplicationInsightsLoggingEnabled"] = bool.TrueString);
+
+                return otBuilder;
             }
 
-            return services;
+            return default;
         }
     }
 }
